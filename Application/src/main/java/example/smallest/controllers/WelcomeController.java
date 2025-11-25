4adc04bc1fb137cba61ec5d0b9bec0edd5ef3125
narayana <80382232+liberty492@users.noreply.github.com>
2025-11-25 16:10:40 +0530

    Update text
    Update text

--- Application/src/main/java/example/smallest/controllers/WelcomeController.java
+++ Application/src/main/java/example/smallest/controllers/WelcomeController.java
@@ -11,9 +11,7 @@ public class WelcomeController {
 	@RequestMapping(method = RequestMethod.GET, produces = {"text/html"})
 	public @ResponseBody String helloWorld() {
 
-		return "<p style=\"font-size:24px; font-weight:bold;\"><div class="center">
-  <B>Evolving <i>Cloud</i> Engineer</B>
-</div> </p>";
+		return "<p style=\"font-size:24px; font-weight:bold;\">🎉 Congratulations DevOps Engineers!!! AWS DevOps is a recession-proof IT Career 💼🚀. All the best for your Bright future 🌟🎯</p>";
 
 	}
 }
